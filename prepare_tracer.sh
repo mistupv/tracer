ab5a31084a404e41ddac6901a027381b3b8595f0
Adrian Palacios <ukonat@gmail.com>
2018-06-18 14:51:32 +0200

    Change example execution in script

--- prepare_tracer.sh
+++ prepare_tracer.sh
@@ -3,4 +3,4 @@ cp ebin/* examples/
 cd examples
 erl -sname edd_main -setcookie edd_cookie
 # put(modules_to_instrument,[]),edd_trace:trace("client_server:main()",10000,self(),".").
-# edd_trace:trace("client_server:main()",10000,self(),".").
+# edd_trace:trace("client_server:main()", self()).
