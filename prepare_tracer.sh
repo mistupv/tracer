2220c5dab3113e16555aa7bd1e839b20763b443c
Adrian Palacios <ukonat@gmail.com>
2018-06-19 12:38:11 +0200

    Add ebin folder to path, updated example use

--- prepare_tracer.sh
+++ prepare_tracer.sh
@@ -1,6 +1,4 @@
 make
-cp ebin/* examples/
-cd examples
-erl -sname edd_main -setcookie edd_cookie
+erl -sname edd_main -setcookie edd_cookie -pa ebin
 # put(modules_to_instrument,[]),edd_trace:trace("client_server:main()",10000,self(),".").
-# edd_trace:trace("client_server:main()", self()).
+# edd_trace:trace("client_server:main()", self(), [{dir, "examples"}]).
